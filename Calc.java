fd9e0ff4b8395bc6a739efafb7e355cb7adaccd9
chetan-172 <156195135+chetan-172@users.noreply.github.com>
2024-02-13 10:29:25 +0530

    Extend of Calculator

---
+++ Calc.java
@@ -0,0 +1,11 @@
+class Calc{
+    float square(float num1){
+        return num1*num1;
+    }
+    float cube(float num1){
+        return num1*num1*num1;
+    }
+    float mod(float num1,float num2){
+        return num1%num2;
+    }
+}
